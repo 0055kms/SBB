0d583e3535615affb480f8ec0adf948f2683352a
kms <dooly6276@gmail.com>
2024-04-15 05:14:03 +0900

    Create entity1

---
+++ src/main/java/com/mysite/sbb/entity/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createTime;
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/mysite/sbb/entity/Question.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createTime;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+}
